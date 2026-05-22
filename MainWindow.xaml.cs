e0d43cd4e93f27a30e3c51a0d64551d2348ed04e
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 23:53:18 +0530

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,27 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.Web.WebView2.Core;
 using System;
 using System.Collections.Generic;
 
-namespace MinimalBrowser;
+namespace TB_Browser;
 
 public sealed partial class MainWindow : Window
 {
-    private readonly Dictionary<string, (string Url, string Title)> _tabData = new();
+    private readonly Dictionary<string, string> _tabUrls = new();
     private string? _currentTabId;
-    private bool _isInitialized;
 
     public MainWindow()
     {
         InitializeComponent();
         TabView.SelectionChanged += TabView_SelectionChanged;
-        WebView.CoreWebView2InitializationCompleted += WebView_InitializationCompleted;
         TabView_AddTabButtonClick(null, null);
-    }
-
-    private void WebView_InitializationCompleted(WebView2 sender, CoreWebView2InitializationCompletedEventArgs args)
-    {
-        _isInitialized = true;
-        if (_currentTabId != null) NavigateToCurrentTab();
     }
 
     private void TabView_AddTabButtonClick(TabView sender, object? args)
     {
         var id = Guid.NewGuid().ToString();
         var tab = new TabViewItem { Header = "New Tab", Tag = id };
-        _tabData[id] = ("https://www.google.com", "New Tab");
+        _tabUrls[id] = "https://www.google.com";
         sender.TabItems.Add(tab);
         sender.SelectedItem = tab;
     }
@@ -38,7 +29,7 @@ public sealed partial class MainWindow : Window
     private void TabView_TabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
     {
         var id = (string)args.Tab.Tag;
-        _tabData.Remove(id);
+        _tabUrls.Remove(id);
         sender.TabItems.Remove(args.Tab);
         if (sender.TabItems.Count == 0) TabView_AddTabButtonClick(sender, null);
     }
@@ -48,21 +39,13 @@ public sealed partial class MainWindow : Window
         if (TabView.SelectedItem is TabViewItem tab)
         {
             _currentTabId = (string)tab.Tag;
-            if (_isInitialized) NavigateToCurrentTab();
+            UrlBox.Text = _tabUrls[_currentTabId];
+            WebView.Source = new Uri(_tabUrls[_currentTabId]);
         }
     }
 
-    private void NavigateToCurrentTab()
-    {
-        if (_currentTabId == null) return;
-        var data = _tabData[_currentTabId];
-        UrlBox.Text = data.Url;
-        ((TabViewItem)TabView.SelectedItem!).Header = data.Title;
-        WebView.CoreWebView2?.Navigate(data.Url);
-    }
-
-    private void BackBtn_Click(object sender, RoutedEventArgs e) => WebView.CoreWebView2?.GoBack();
-    private void FwdBtn_Click(object sender, RoutedEventArgs e) => WebView.CoreWebView2?.GoForward();
+    private void BackBtn_Click(object sender, RoutedEventArgs e) => WebView.GoBack();
+    private void FwdBtn_Click(object sender, RoutedEventArgs e) => WebView.GoForward();
     private void GoBtn_Click(object sender, RoutedEventArgs e) => Navigate();
     private void UrlBox_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
     {
@@ -73,7 +56,7 @@ public sealed partial class MainWindow : Window
     {
         var url = UrlBox.Text.Trim();
         if (!url.StartsWith("http")) url = "https://" + url;
-        WebView.CoreWebView2?.Navigate(url);
-        if (_currentTabId != null) _tabData[_currentTabId] = (url, "Loading...");
+        if (_currentTabId != null) _tabUrls[_currentTabId] = url;
+        WebView.Source = new Uri(url);
     }
 }
