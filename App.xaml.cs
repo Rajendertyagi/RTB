e29f5143a4b3b776865744955298928f4eeca545
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 00:10:21 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,9 @@
 using Microsoft.UI.Xaml;
-
 namespace TB_Browser;
 
 public partial class App : Application
 {
     public App() => InitializeComponent();
-
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         var window = new MainWindow();
