286cd7c7ec55ba92c0c81fa95d6b949992d3bcbf
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 16:20:55 +0530

    Update IBrowserService.cs

--- Core/Services/IBrowserService.cs
+++ Core/Services/IBrowserService.cs
@@ -1,12 +1,13 @@
 using System;
 using Microsoft.Web.WebView2.Core;
+using TB_Browser.Core.Logging;
 
 namespace TB_Browser.Core.Services
 {
     public interface IBrowserService
     {
         ITabService TabService { get; set; }
-        void SetWebView(CoreWebView2 webView); // ✅ ADD THIS
+        void SetWebView(CoreWebView2 webView);
         void Navigate(string url);
         void GoBack();
         void GoForward();
