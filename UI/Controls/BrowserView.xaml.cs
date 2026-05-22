cd7069bbecae9e11c8e2434f3f7689da17374392
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 15:37:22 +0530

    Update BrowserView.xaml.cs

--- UI/Controls/BrowserView.xaml.cs
+++ UI/Controls/BrowserView.xaml.cs
@@ -1,6 +1,26 @@
-<UserControl x:Class="TB_Browser.UI.Controls.BrowserView"
-             xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-             xmlns:wv2="using:Microsoft.Web.WebView2.Core">
-    <ContentControl x:Name="WebViewHost"/>
-</UserControl>
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Wpf;
+using TB_Browser.Core.Models;
+using TB_Browser.Core.Services;
+
+namespace TB_Browser.UI.Controls
+{
+    public partial class BrowserView : UserControl
+    {
+        private readonly IBrowserService _svc;
+        public BrowserView(IBrowserService svc)
+        {
+            InitializeComponent();
+            _svc = svc;
+        }
+
+        public async void SwitchTo(TabModel tab)
+        {
+            var wv = new WebView2();
+            WebViewHost.Content = wv;
+            await wv.EnsureCoreWebView2Async();
+            _svc.SetWebView(wv.CoreWebView2);
+            wv.Source = new System.Uri(tab.Url);
+        }
+    }
+}
