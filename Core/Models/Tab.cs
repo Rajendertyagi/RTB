677d06de0526fea39d17d712295602ba1c366cbe
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 23:53:56 +0530

    Delete Core directory

--- Core/Models/Tab.cs
+++
@@ -1,8 +0,0 @@
-namespace TB_Browser.Core.Models;
-
-public class Tab
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = "New Tab";
-    public string Url { get; set; } = "https://www.google.com";
-}
