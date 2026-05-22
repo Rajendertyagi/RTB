19b9d3a1faaedc0ef7e437f6fd3b81c84e857276
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 16:10:02 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,21 +9,13 @@ namespace TB_Browser
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            // 1. Services
             var tabService = new TabService();
             var browserService = new BrowserService();
             browserService.TabService = tabService;
-
-            // 2. UI Controls
             var tabBar = new TabBar(tabService);
             var addressBar = new AddressBar(browserService);
             var browserView = new BrowserView(browserService);
-
-            // 3. Bind tab changes
             tabService.ActiveTabChanged += (s, tab) => browserView.SwitchTo(tab);
-
-            // 4. Show window
             var mainWindow = new MainWindow(tabBar, addressBar, browserView);
             mainWindow.Show();
         }
