47c6c114a09339af846081e8c0874d0ef74ca714
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 15:21:54 +0530

    Create TabModel.cs

---
+++ Core/Models/TabModel.cs
@@ -0,0 +1,12 @@
+using Microsoft.Web.WebView2.Core;
+
+namespace TB_Browser.Core.Models
+{
+    public class TabModel
+    {
+        public int Id { get; init; }
+        public string Title { get; set; } = "New Tab";
+        public string Url { get; set; } = "https://www.bing.com";
+        public CoreWebView2? WebView { get; set; }
+    }
+}
