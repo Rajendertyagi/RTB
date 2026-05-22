2661f8eb22d70734e055394d2d59b8f40d171a2f
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 17:53:44 +0530

    Update AddressBar.xaml.cs

--- UI/Controls/AddressBar.xaml.cs
+++ UI/Controls/AddressBar.xaml.cs
@@ -1,7 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using TB_Browser.Core.Logging;
 using TB_Browser.Core.Services;
 
 namespace TB_Browser.UI.Controls
@@ -9,11 +8,13 @@ namespace TB_Browser.UI.Controls
     public partial class AddressBar : UserControl
     {
         private readonly IBrowserService _svc;
-        public AddressBar(IBrowserService svc) { InitializeComponent(); _svc = svc; _svc.UrlChanged += (_, u) => UrlBox.Text = u; }
-        private void Back_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Back"); _svc.GoBack(); }
-        private void Forward_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Forward"); _svc.GoForward(); }
-        private void Reload_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Reload"); _svc.Reload(); }
-        private void Go_Click(object s, RoutedEventArgs e) { Logger.Info("UI", $"Go: {UrlBox.Text}"); _svc.Navigate(UrlBox.Text); }
-        private void Url_KeyDown(object s, KeyEventArgs e) { if (e.Key == Key.Enter) { Logger.Info("UI", $"Enter: {UrlBox.Text}"); _svc.Navigate(UrlBox.Text); } }
+        public AddressBar(IBrowserService svc) { InitializeComponent(); _svc = svc; _svc.UrlChanged += (_, u) => UrlBox.Text = u; _svc.CanGoBackChanged += (_, b) => BackBtn.IsEnabled = b; _svc.CanGoForwardChanged += (_, b) => FwdBtn.IsEnabled = b; }
+        private void Back_Click(object s, RoutedEventArgs e) => _svc.GoBack();
+        private void Forward_Click(object s, RoutedEventArgs e) => _svc.GoForward();
+        private void Reload_Click(object s, RoutedEventArgs e) => _svc.Reload();
+        private void Go_Click(object s, RoutedEventArgs e) => _svc.Navigate(UrlBox.Text);
+        private void Url_KeyDown(object s, KeyEventArgs e) { if (e.Key == Key.Enter) _svc.Navigate(UrlBox.Text); }
+        private void Url_TextChanged(object s, TextChangedEventArgs e) { if (!UrlBox.IsFocused) return; }
+        public void FocusUrl() { UrlBox.Focus(); UrlBox.SelectAll(); }
     }
 }
