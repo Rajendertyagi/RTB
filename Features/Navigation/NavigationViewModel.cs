73ff71278456468a4446bb8e6ed4da69777f683e
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 21:39:46 +0530

    Delete Features directory

--- Features/Navigation/NavigationViewModel.cs
+++
@@ -1,8 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace TB.Features.Navigation;
-
-public partial class NavigationViewModel : ObservableObject
-{
-    [ObservableProperty] private string _addressBarText = string.Empty;
-}
