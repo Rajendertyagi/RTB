5aefe0a27968b82ba32a6ae64e130bbc178de533
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 17:55:53 +0530

    Create PathResolver.cs

---
+++ Infrastructure/PathResolver.cs
@@ -0,0 +1,8 @@
+using System;
+using System.IO;
+namespace TB.Infrastructure;
+public class PathResolver
+{
+    public string DataDir { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TB");
+    public void EnsureDirectories() => Directory.CreateDirectory(DataDir);
+}
