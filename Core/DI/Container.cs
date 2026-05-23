90c856c24860fc224767383f365bc300f7de6013
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 16:06:17 +0530

    Create Container.cs

---
+++ Core/DI/Container.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.DependencyInjection;
+using TB.Data.Services;
+using TB.Features.Navigation;
+using TB.Features.Tabs;
+using TB.Infrastructure;
+
+namespace TB.Core.DI;
+
+public static class Container
+{
+    public static IServiceProvider Register(PathResolver pathResolver)
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(pathResolver);
+        services.AddSingleton<IDbConnectionFactory>(_ => new DbConnectionFactory(pathResolver));
+        services.AddSingleton<DbInitializer>();
+        services.AddSingleton<SettingsService>();
+        services.AddSingleton<NavigationViewModel>();
+        services.AddSingleton<TabService>();
+        return services.BuildServiceProvider();
+    }
+}
