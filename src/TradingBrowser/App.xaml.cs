0caedc68fbadc348c7075c01a1334812f78dfff9
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-24 13:57:22 +0530

    Update App.xaml.cs

--- src/TradingBrowser/App.xaml.cs
+++ src/TradingBrowser/App.xaml.cs
@@ -1,16 +1,30 @@
 using Microsoft.UI.Xaml;
+using TradingBrowser.Services;
+using System;
 
 namespace TradingBrowser;
 
 public partial class App : Application
 {
     private Window? _window;
+    public static DatabaseService? Db { get; private set; }
 
     public App() => this.InitializeComponent();
 
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
-        _window = new MainWindow();
-        _window.Activate();
+        try
+        {
+            LoggingService.Log("App startup initiated.");
+            Db = new DatabaseService();
+            
+            _window = new MainWindow();
+            _window.Activate();
+        }
+        catch (Exception ex)
+        {
+            LoggingService.Error("Fatal error during app startup", ex);
+            throw;
+        }
     }
 }
